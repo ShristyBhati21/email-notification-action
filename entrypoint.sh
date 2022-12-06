77fe4c51700f1e5650572cb29e56db7f974b0f8b
ShristyBhati21 <shristy.bhati@byzgen.com>
2022-12-06 11:21:58 +0000

    First email test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+USERNAME="bhati.shristy@gmail.com"
+PASSWORD="abc"
+SMTP_PORT=465
+SMTP_SERVER="smtp.gmail.com"
+message = """\
+          Subject: GitHub Email Report
+          This is your daily email report.
+          """
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(USERNAME,PASSWORD)
+    server.sendmail(USERNAME,USERNAME,message)
