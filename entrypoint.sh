021dab188942beb5d75247945879b33c2f5a8c64
ShristyBhati21 <shristy.bhati@byzgen.com>
2022-12-09 15:36:58 +0000

    new test

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ sendemail -f ${INPUT_FROM_EMAIL} \
           -o tls=yes \
           -u ${INPUT_SUBJECT} \
           -xu ${INPUT_AUTHORISED_USERNAME} \
-          -xp ${System.getenv("PASSWORD")} \
+          -xp ${INPUT_AUTHORISED_PASSWORD} \
           -o message-content-type=html \
           -o message-file=/message.html
