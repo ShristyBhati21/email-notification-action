d8873e401e69ea1ebf665ec53679dcae25eb6ed9
ShristyBhati21 <shristy.bhati@byzgen.com>
2022-12-09 15:50:41 +0000

    My first action is ready

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,23 @@
 #!/bin/sh -l
 
+
+set -x
+
+echo "JOB_STATUS: ${INPUT_JOB_STATUS}"
+
+# curl request for the slack webhook webhook: curl POST, headers, data (slack json obj), slack webhook
+if [[ "${INPUT_JOB_STATUS}" == 'success' ]]; then
+  export INPUT_SLACK_STATUS_EMOJI=':white_check_mark::white_check_mark::white_check_mark:'
+  export INPUT_DISPLAY_IMAGE_URL='https://www.gifcen.com/wp-content/uploads/2022/02/congratulations-gif-8.gif'
+
+elif [[ "${INPUT_JOB_STATUS}" == 'failure' ]]; then
+  export INPUT_SLACK_STATUS_EMOJI=':rotating_light::rotating_light::rotating_light:'
+  export INPUT_DISPLAY_IMAGE_URL='https://c.tenor.com/-kZOB16tELEAAAAC/this-is-fine-fire.gif'
+
+fi # else cancelled
+
+curl -X POST -H 'Content-type: application/html' --data "$(envsubst < /message.html)" "${INPUT_SLACK_WEBHOOK}"
+
 sendemail -f ${INPUT_FROM_EMAIL} \
           -t ${INPUT_TO_EMAIL} \
           -s "smtp.gmail.com:587" \
