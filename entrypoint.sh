c139102d24b5649e6cd1c319975210dacdce0369
ShristyBhati21 <shristy.bhati@byzgen.com>
2022-12-12 16:13:28 +0000

    new test

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,7 +23,7 @@ sendemail -f ${INPUT_FROM_EMAIL} \
           -s "smtp.gmail.com:587" \
           -v -v \
           -o tls=yes \
-          -u ${INPUT_JOB_STATUS} : ${INPUT_SUBJECT} \
+          -u '\033[1m${INPUT_JOB_STATUS}\033[0m' : ${INPUT_SUBJECT} \
           -xu ${INPUT_AUTHORISED_USERNAME} \
           -xp ${INPUT_AUTHORISED_PASSWORD} \
           -o message-content-type=html \
